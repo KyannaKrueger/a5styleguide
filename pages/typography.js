73e1c57cc656118311d4fe74dd0f2fe44caeb8a3
GloGlorGloria <gloriabcit@gmail.com>
2024-04-11 22:06:05 -0700

    revised css

--- pages/typography.js
+++ pages/typography.js
@@ -26,35 +26,53 @@ export default function Typography() {
             Another reason why we chose this font is because  <span className={styles.orange}>SF Pro supports for over 150 languages</span> across various scripts ensures inclusivity for a diverse user base.</p>
         </div>
         <h1 className={styles.bigSF}>SF Pro</h1>
+        
+        <div className={styles.ExContainer}>
+          <h3 className={styles.characterH}>Example Characters</h3>
           <div className={styles.mainExContainer}>
-              <h3 className={styles.characterH}>Example Characters:</h3>
-              <div className={styles.characterExamples}>
-                <h4 className={styles.characters}>Regular</h4>
-                <p className={styles.characters}>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br/>abcdefghijklmnopqristuvwxyz<br/>1234567890</p>
+              <div className={styles.characterLight}>
+                <h4 className={styles.charactersLight}>Light</h4>
+                <p className={styles.charactersLight}>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br/>abcdefghijklmnopqristuvwxyz<br/>1234567890</p>
+              </div>
+              <div className={styles.characterRegular}>
+                <h4 className={styles.charactersRegular}>Regular</h4>
+                <p className={styles.charactersRegular}>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br/>abcdefghijklmnopqristuvwxyz<br/>1234567890</p>
+              </div>
+              <div className={styles.characterSemibold}>
+                <h4 className={styles.charactersSemi}>Semi-bold</h4>
+                <p className={styles.charactersSemi}>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br/>abcdefghijklmnopqristuvwxyz<br/>1234567890</p>
+              </div>
+              <div className={styles.characterBold}>
+                <h4 className={styles.charactersBold}>Bold</h4>
+                <p className={styles.charactersBold}>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br/>abcdefghijklmnopqristuvwxyz<br/>1234567890</p>
               </div>
           </div>
-          <div className={styles.fontSize}>
-        <h3 className={styles.headingH}>Heading sizes:</h3>
-        <p className={styles.hOne}>SF Pro Bold 38px</p>
-        <p className={styles.hTwo}>SF Pro Bold 32px</p>
-       
-        <div className={styles.subContainer}>
+          </div>
+
+        <div className={styles.fontSize}>
+          <div className={styles.subContainer}>
+            <h3 className={styles.headingH}>Heading</h3>
+              <p className={styles.hOne}>SF Pro Bold 38px</p>
+              <p className={styles.hTwo}>SF Pro Bold 32px</p>
+          </div>
           <div className={styles.subConOne}>
             <h3 className={styles.sec}>Section head</h3>
             <p className={styles.secH}>SF Pro Bold 24px</p>
+            <p className={styles.secH2}>SF Pro Bold 18px</p>
           </div>
           <div className={styles.subCon}>
             <h3 className={styles.secSub}>Sub-section head</h3>
-            <p className={styles.sub1}>SF Pro Bold 18px</p>
+            <p className={styles.sub3}>SF Pro Bold 16px</p>
             <p className={styles.sub2}>SF Pro Semi-bold 16px</p>
           </div>
           <div className={styles.bodyContainer}>
             <h3 className={styles.bodyCopy}>Body copy</h3>
-            <p className={styles.bodyOne}>SF Pro Regular 16px</p>
-            <p className={styles.bodyTwo}>SF Pro Regular 14px</p>
+            <div className={styles.bodyFont}>
+              <p className={styles.bodyOne}>SF Pro Regular 16px</p>
+              <p className={styles.bodyTwo}>SF Pro Regular 14px</p>
+            </div>
           </div>
           </div>
-        </div>
         </main>
         <Footer/>
       </>
